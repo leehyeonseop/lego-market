effa35038b0aa7f0ded2e099ea9b2e13c632a31f
LeeHyeonSeop <gustjq4146@naver.com>
2022-07-06 11:35:09 +0900

    현섭 수정 테스트

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 
 const Home = () => {
-  return <HomeDiv>라우터 , 컴포넌트 테스트 페이지 입니다</HomeDiv>;
+  return <HomeDiv>라우터 , 컴포넌트 테스트 페이지 입니다 내용수정(현섭)</HomeDiv>;
 };
 
 const HomeDiv = styled.div`
